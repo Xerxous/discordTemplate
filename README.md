b4f40ff27b420177806b9594cec6d9c143486835
jackson <woosuk2009@gmail.com>
2017-06-15 23:23:00 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# discordTemplate
+A jump start for discord bots(discord.js)
+
+## Setting up
+- `npm install`
+- `npm install -g node, babel-cli, babel-node, forever`
+- JS - es6
+- Change config.json to your needs
+- Add secret.json with {API_KEY: "YOUR API_KEY"} in the main directory ./(where config.json is)
+- Add modules to modules.json
+- Create new folders/js files/json files for additional modules: an example has been made in the modules folder
+
+# Starting
+* `npm start` - nodemon driven development environment
+* `npm run compile` - babel build for prod
+* `npm run prod` - forever run on production
---
+++ app.js
@@ -0,0 +1,55 @@
+import Discord from 'discord.js';
+import modules from './lib/initializer';
+import config from './config';
+import secret from './secret';
+import colors from 'colors';
+
+const client = new Discord.Client();
+let environment;
+let broadcast;
+let defaultChatChannel;
+let defaultVoiceChannel;
+let mods;
+
+// Configures all settings and environment variables
+client.on('ready', () => {
+  console.log(`***${config.botName} is online***`.cyan.underline.bold);
+  defaultChatChannel = client.channels.find('name', config.defaultChatChannel);
+  defaultVoiceChannel = client.channels.find('name', config.defaultVoiceChannel);
+  mods = modules(config.commandPrefix);
+  if (defaultVoiceChannel) {
+    broadcast = client.createVoiceBroadcast();
+    defaultVoiceChannel.join();
+    console.log(`joined ${config.defaultVoiceChannel} voice channel`.yellow);
+  }
+  // expand the environment variable as necessary
+  environment = {
+    client,
+    config,
+    broadcast,
+    defaultChatChannel,
+    defaultVoiceChannel
+  }
+});
+
+client.on('message', message => {
+  if (message.author.id != config.botId) {
+    console.log(`Message sent: ${message.content}`);
+    for (let module in mods) {
+      for (let key in mods[module].listens) {
+        let match = message.content.match(`${config.commandPrefix}${key}`);
+        if (match) {
+          console.log(`Found match. Calling ${mods[module].listens[key]}`);
+          mods[module].module[mods[module].listens[key]](environment, message, match);
+          break;
+        }
+      }
+    }
+  }
+});
+
+/*
+ * in secret.json: key API_KEY contains bot api key
+ * create before starting bot
+ */
+client.login(secret.API_KEY);
